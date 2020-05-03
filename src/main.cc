cb26310d9766628c53a448b4fd2be2652f2e759a
Maximilian Bels <maximilianbels@gmail.com>
2020-05-04 00:21:38 +0200

    Configure bochsrc.txt. Create directory for old tasks. Project should now be immediately compilable.

--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,27 @@
-#include "device/CgaChannel.h"
-#include "io/PrintStream.h"
+//#include "device/CgaChannel.h"
+//#include "io/PrintStream.h"
+#include <stdio.h>
 
-CgaChannel cga;		// unser CGA-Ausgabekanal
-PrintStream out(cga);	// unseren PrintStream mit Ausgabekanal verknuepfen
+//CgaChannel cga;		// unser CGA-Ausgabekanal
+//PrintStream out(cga);	// unseren PrintStream mit Ausgabekanal verknuepfen
 
 int main()
 {
+	//out.println("Hello World");
+	//
+	//for(int i = 0; i < 1000; i++) {
+	//	out.print("Dezimal ");
+	//	out.print(i);
+	//	out.print(" Binaer ");
+	//	out.print(i, 2);
+	//	out.print(" Hexadezimal ");
+	//	out.print(i, 16);
+	//	out.println();
+	//}
+	//
+	//out.println("done");
 
-	out.println("Hello World");
-
-	for(int i = 0; i < 1000; i++) {
-		out.print("Dezimal ");
-		out.print(i);
-		out.print(" Binaer ");
-		out.print(i, 2);
-		out.print(" Hexadezimal ");
-		out.print(i, 16);
-		out.println();
-	}
-
-	out.println("done");
-
-	while(1){}
+	while(1) {}
 
 	return 0;
 }
