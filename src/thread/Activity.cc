2e6eaa0e55a46a884aafcf451899107b1248127b
Maximilian Bels <maximilianbels@gmail.com>
2020-05-13 23:50:59 +0200

    Delete tasks folder and old task data (replace by release-tags).

---
+++ src/thread/Activity.cc
@@ -0,0 +1,44 @@
+// belserich on 13.05.20
+
+#include "thread/Activity.h"
+
+Activity::Activity(void *tos) : Coroutine(tos)
+{
+
+}
+
+Activity::Activity()
+{
+
+}
+
+Activity::~Activity()
+{
+
+}
+
+void Activity::sleep()
+{
+
+}
+
+void Activity::wakeup()
+{
+
+}
+
+void Activity::yield()
+{
+
+}
+
+void Activity::exit()
+{
+
+}
+
+void Activity::join()
+{
+
+}
+
---
+++ src/thread/Coroutine.cc
@@ -0,0 +1,13 @@
+// belserich on 13.05.20
+
+#include "thread/Coroutine.h"
+
+void Coroutine::startup(Coroutine *obj)
+{
+
+}
+
+void Coroutine::setup(void *tos)
+{
+
+}
